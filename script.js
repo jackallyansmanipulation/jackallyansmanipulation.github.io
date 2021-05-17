a6ca8daf956210e9d321ba157209a48a797f7496
jackallyansmanipulation <84206327+jackallyansmanipulation@users.noreply.github.com>
2021-05-17 14:00:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function op1(){
-	document.getElementById('o1').style.color = '#13009B';
+	document.getElementById('o1').style.color = '#0034C2';
 	document.getElementById('o2').style.color = '#000000';
 	document.getElementById('ebook').style.display = 'block';
 	document.getElementById('info').style.display = 'none';
 }
 function op2(){
-	document.getElementById('o2').style.color = '#13009B';
+	document.getElementById('o2').style.color = '#0034C2';
 	document.getElementById('o1').style.color = '#000000';
 	document.getElementById('info').style.display = 'block';
 	document.getElementById('ebook').style.display = 'none';
