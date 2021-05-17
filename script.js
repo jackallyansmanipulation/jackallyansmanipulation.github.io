b16da6bce2136fbeb5dffce16bacd3033805172a
jackallyansmanipulation <84206327+jackallyansmanipulation@users.noreply.github.com>
2021-05-17 13:51:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function op1(){
+	document.getElementById('o1').style.color = '#002A7F';
+	document.getElementById('o2').style.color = '#000000';
+	document.getElementById('ebook').style.display = 'block';
+	document.getElementById('info').style.display = 'none';
+}
+function op2(){
+	document.getElementById('o2').style.color = '#002A7F';
+	document.getElementById('o1').style.color = '#000000';
+	document.getElementById('info').style.display = 'block';
+	document.getElementById('ebook').style.display = 'none';
+}
